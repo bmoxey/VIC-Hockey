7de2d697c2c417341326ef52f9740473d839a6d0
bmoxey <bmoxey@gmail.com>
2023-11-07 21:44:10 +1100

    detailed player view

---
+++ VIC Hockey/Views/DetailPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailPlayerView.swift
+//  VIC Hockey
+//
+//  Created by Brett Moxey on 7/11/2023.
+//
+
+import SwiftUI
+
+struct DetailPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailPlayerView()
+}
