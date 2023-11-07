60e087924d950dfbf693c509d5b771cb898968ef
bmoxey <bmoxey@gmail.com>
2023-11-07 14:51:46 +1100

    finished schedule

---
+++ VIC Hockey/Views/DetailScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailScheduleView.swift
+//  VIC Hockey
+//
+//  Created by Brett Moxey on 6/11/2023.
+//
+
+import SwiftUI
+
+struct DetailScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailScheduleView()
+}
---
+++ VIC Hockey/Views/DetailSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailSearchView.swift
+//  VIC Hockey
+//
+//  Created by Brett Moxey on 6/11/2023.
+//
+
+import SwiftUI
+
+struct DetailSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailSearchView()
+}
