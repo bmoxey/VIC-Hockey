90e8d9d97530b4108a89311a9133afc86ce22cbe
bmoxey <bmoxey@gmail.com>
2023-11-08 16:48:48 +1100

    updated ladder and game views

---
+++ VIC Hockey/Views/Ladder/DetailLadderHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailLadderHeaderView.swift
+//  VIC Hockey
+//
+//  Created by Brett Moxey on 8/11/2023.
+//
+
+import SwiftUI
+
+struct DetailLadderHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailLadderHeaderView()
+}
---
+++ VIC Hockey/Views/Ladder/DetailLadderItemView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailLadderItemView.swift
+//  VIC Hockey
+//
+//  Created by Brett Moxey on 8/11/2023.
+//
+
+import SwiftUI
+
+struct DetailLadderItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailLadderItemView()
+}
